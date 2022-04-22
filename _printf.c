92a41dbbb836b4d98a318980b080893a7f7145cb
Boye <akingbelureadeboye18@yahoo.com>
2022-04-22 07:52:17 -0700

    edit

--- _printf.c
+++ _printf.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  *_printf - prints formatted output.
-  *@format: input.
+  * _printf - prints formatted output.
+  * @format: input.
   *
-  *Return: number of chars printed or -1.
+  * Return: number of chars printed or -1.
   */
 
 int _printf(const char *format, ...)
